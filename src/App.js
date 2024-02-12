e58c7d6ee7b1384c499ef665fffb80edb2612731
@coder shruti <www.meshruti@gmail.com>
2024-02-13 02:36:18 +0530

    basic_reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./styles/App.css"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+
+const App = () => {
+  return <Router>
+    <Header />
+    <Routes>
+
+     <Route path="/" element={<Home/>} />
+     <Route path="/about" element={<About/>} />
+     <Route path="/contact" element={<Contact/>} />
+     <Route path="*" element={<div>Page not found 404</div>} />
+
+    </Routes>
+  </Router>
 }
 
-export default App;
+export default App
